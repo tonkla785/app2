6feaa94ba138dcacc186936d55ebbf2db8d82d13
tonkla785 <tonkla785@gmail.com>
2024-08-12 20:08:31 +0700

    button

---
+++ src/class-components.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class Button extends Component {
+    showAlert(msg) {
+        alert(msg);
+    }
+
+    onClickButtonOK = () => {
+        this.showAlert("Hello");
+    };
+
+    render() {
+        return (
+            <button onClick={this.onClickButtonOK}>
+                OK
+            </button>
+        );
+    }
+}
